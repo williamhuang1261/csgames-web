caefb1e5fb86e5a06ac9c4744c83ef8386e406a5
William Huang <william.huang.1261@ulaval.ca>
2026-03-07 20:06:16 -0500

    show easter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {useEffect, useState} from "react";
 
 function App() {
     const [x, setX] = useState(150);
+    const [showEaster, setShowEaster] = useState(false);
 
     useEffect(() => {
         const dragEnter = (e: DragEvent) => {
@@ -15,7 +16,7 @@ function App() {
         document.addEventListener('dragenter', dragEnter)
 
 
-        document.addEventListener("dragstart", function( event ) {
+        document.addEventListener("dragstart", function (event) {
             const img = new Image();
             img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
             // @ts-expect-error jasfkl;ajsdf
@@ -26,7 +27,7 @@ function App() {
 
     useEffect(() => {
         const dragFn = (e: DragEvent) => {
-            if (e.x != 0 ) {
+            if (e.x != 0) {
                 setX(e.x - 32)
             }
         }
@@ -50,18 +51,32 @@ function App() {
 
 
     return (
-        <div className={"app-layout"}>
+
+        showEaster
+            ?
+            <div className={"easter-back"}>
+                <div>
+                <button className={"reload button"} onClick={() => location.reload()}>Reload page</button>
+                <img src={"/Credz.png"}  alt={"By William H and Charles RD - Universite Laval Rouge"} width={500}/>
+                </div>
+            <video width={800} height={400} autoPlay={true} controls={true}>
+                <source src={'/a.mp4'} type={'video/mp4'}/>
+            </video>
+            </div>
+
+            : <div className={"app-layout"}>
             <ul className={"text-buttons"} style={{
                 width: x,
             }}>
                 <li>🌟{x >= 100 && <span>Game Page</span>}</li>
-                <li>🐣{x >= 100 && <span>Easter egg</span>}</li>
+                <li onClick={() => setShowEaster(true)} className={'easter-button'}>🐣{x >= 100 && <span>Easter egg</span>}</li>
             </ul>
             <div className={"game-layout"}>
                 <div className={"drag-bar"} draggable={true}></div>
                 <Game/>
             </div>
         </div>
+
     );
 }
 
