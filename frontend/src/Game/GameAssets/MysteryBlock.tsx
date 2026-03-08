ba68bdbcc3710758c59a9a5f53997a40af2cac21
William Huang <william.huang.1261@ulaval.ca>
2026-03-07 20:23:53 -0500

    show gif

--- frontend/src/Game/GameAssets/MysteryBlock.tsx
+++ frontend/src/Game/GameAssets/MysteryBlock.tsx
@@ -1,6 +1,10 @@
 import { Camera } from '../Camera.tsx';
 import mysteryBlock from '../../assets/scenery/special/myster-box-animated.gif';
 import type { Coordinates, GameAsset } from './GameAsset.tsx';
+import f0 from "../../../public/gif/frame_0_delay-0.2s.gif"
+import f1 from "../../../public/gif/frame_1_delay-0.2s.gif"
+import f2 from "../../../public/gif/frame_2_delay-0.2s.gif"
+import f3 from "../../../public/gif/frame_3_delay-0.2s.gif"
 
 export class MysteryBlock implements GameAsset {
     sprite: HTMLImageElement;
@@ -11,6 +15,10 @@ export class MysteryBlock implements GameAsset {
     pos: Coordinates;
 
     useCamera: boolean = true;
+
+    counter = 0;
+    slower = 0;
+    images: HTMLImageElement["src"][] = [f0, f1, f2, f3]
 
     constructor(width: number, height: number) {
         this.pos = { x: 200, y: 200 };
@@ -31,6 +39,17 @@ export class MysteryBlock implements GameAsset {
 
     render(ctx: CanvasRenderingContext2D): void {
         const camera = Camera.getInstance();
+
+
+
+        const image: HTMLImageElement["src"] =  this.images[this.counter]
+
+        this.slower++
+        if (this.slower > 400) {
+            this.counter = (this.counter + 1) % 4
+        }
+        this.sprite = new Image()
+        this.sprite.src = image
 
         ctx.drawImage(
             this.sprite,
